f7a22ec5d7a7c803619b5991c28701ca99717274
omkarmali4437 <omkarmalli688@gmail.com>
2021-03-25 20:06:30 +0530

    Added logic for doing data manipulation on salary

--- src/main/java/com/myPackage/EmployeePayroll.java
+++ src/main/java/com/myPackage/EmployeePayroll.java
@@ -2,7 +2,6 @@ package com.myPackage;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 
 public class EmployeePayroll {
@@ -33,10 +32,10 @@ public class EmployeePayroll {
                 String gender=resultSet.getString(5);
                 System.out.println("+++++++++++++++++++++++++++");
                 System.out.println("Id: "+id);
-                System.out.println("FirstName: "+name);
-                System.out.println("LastName: "+date);
-                System.out.println("Adress: "+salary);
-                System.out.println("City: "+gender);
+                System.out.println("Name: "+name);
+                System.out.println("Start date: "+date);
+                System.out.println("Salary: "+salary);
+                System.out.println("Gender: "+gender);
 
                 EmployeePayrollData employeePayrollData=new EmployeePayrollData(resultSet.getInt(1),resultSet.getString(2),
                         resultSet.getDate(3),resultSet.getDouble(4),resultSet.getString(5));
@@ -80,10 +79,10 @@ public class EmployeePayroll {
                 String gender=resultSet.getString(5);
                 System.out.println("+++++++++++++++++++++++++++");
                 System.out.println("Id: "+id);
-                System.out.println("FirstName: "+name);
-                System.out.println("LastName: "+date);
-                System.out.println("Adress: "+salary);
-                System.out.println("City: "+gender);
+                System.out.println("Name: "+name);
+                System.out.println("Start Date: "+date);
+                System.out.println("Salary: "+salary);
+                System.out.println("Gender: "+gender);
 
                 EmployeePayrollData employeePayrollData=new EmployeePayrollData(resultSet.getInt(1),resultSet.getString(2),
                         resultSet.getDate(3),resultSet.getDouble(4),resultSet.getString(5));
@@ -94,5 +93,29 @@ public class EmployeePayroll {
             throwables.printStackTrace();
         }
         return employeePayrollDataArrayList;
+    }
+
+    public List<String> dataManipulation(){
+        List<String> list=new ArrayList();
+        try {
+            Connection connection=this.getConnection();
+            PreparedStatement preparedStatement=connection.prepareStatement("select gender,sum(salary), avg(salary),min(salary),max(salary),count(salary) from employee_payroll group by gender; ");
+            ResultSet resultSet=preparedStatement.executeQuery();
+            while (resultSet.next()){
+                int index=1;
+                System.out.println("Gender: "+resultSet.getString(1));
+                System.out.println("Salary: "+resultSet.getString(2));
+                for (int i=0;i<13;i++){
+                    if(index<7) {
+                        list.add(i, resultSet.getString(index));
+                        index++;
+                    }
+                }
+                System.out.println(list);
+            }
+        }catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+        return list;
     }
 }
--- src/test/java/com/myPackage/EmployeePayrollTest.java
+++ src/test/java/com/myPackage/EmployeePayrollTest.java
@@ -1,32 +1,39 @@
 package com.myPackage;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.sql.*;
-import java.util.Enumeration;
 import java.util.List;
 
 
 public class EmployeePayrollTest {
+    EmployeePayroll employeePayroll;
+    @Before
+    public void setup(){
+        employeePayroll=new EmployeePayroll();
+    }
 
     @Test
     public void given_select_statement_should_return_count(){
-        EmployeePayroll employeePayroll=new EmployeePayroll();
         List<EmployeePayrollData> employeePayrollDataList=employeePayroll.readData();
-        Assert.assertEquals(3,employeePayrollDataList.size());
+        Assert.assertEquals(4,employeePayrollDataList.size());
     }
 
     @Test
     public void update_table_should_return_true(){
-        EmployeePayroll employeePayroll=new EmployeePayroll();
         long result=employeePayroll.updateData();
         Assert.assertEquals(1,result);
     }
 
     @Test
     public void should_reurn_employee_datails_for_a_given_date_range(){
-        EmployeePayroll employeePayroll=new EmployeePayroll();
         List<EmployeePayrollData>employeePayrollDataList=employeePayroll.employeeDetailsfromDate();
-        Assert.assertEquals(2,employeePayrollDataList.size());
+        Assert.assertEquals(3,employeePayrollDataList.size());
+    }
+
+    @Test
+    public void retur_sum_avg_min_max_count(){
+        List<String> list=employeePayroll.dataManipulation();
+        Assert.assertEquals(12,list.size());
     }
 }
